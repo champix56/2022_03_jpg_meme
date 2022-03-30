b269398ea471db7f7c1b08ae44af6dc7d0a8a799
champix56 <desorbaix@free.fr>
2022-03-30 13:32:41 +0100

    fonction d'update de svg UI

--- js/index.js
+++ js/index.js
@@ -9,4 +9,17 @@ function lorsDuChargementDeLaPage() {
     baliseJsLoaded.className = "isLoaded";
     console.log('Le js est chargé', baliseJsLoaded);
 }
-document.addEventListener('DOMContentLoaded',lorsDuChargementDeLaPage)
+document.addEventListener('DOMContentLoaded',lorsDuChargementDeLaPage);
+
+function setMemeOnSVGViewer(meme) {
+    //gestion d'un valeur par default
+    if(undefined===meme){
+        meme=new Meme();
+        meme.setDummyValues();
+    }
+    //selection du noeud text enfant de svg
+    var svgTextNode=document.querySelector('svg>text');
+    // moddif du contenu HTML de la balise 
+    svgTextNode.innerHTML=meme.text;
+    
+}
