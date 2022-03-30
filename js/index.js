bbd9df7f3eef4ef5fdf8738da78cb44aa4438d67
champix56 <desorbaix@free.fr>
2022-03-30 10:28:51 +0100

    avec ecouteur devennement

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
-//selection de la balise a manipuler
-var baliseJsLoaded=document.querySelector("#isJsLoaded");
-//edition dui contenu de la balise contenu dans la avriable
-baliseJsLoaded.innerHTML="JS loaded";
-baliseJsLoaded.className="isLoaded";
-console.log('Le js est chargé',baliseJsLoaded);
+/**
+ * fonction d'activation du js
+ */
+function lorsDuChargementDeLaPage() {
+    //selection de la balise a manipuler
+    var baliseJsLoaded = document.querySelector("#isJsLoaded");
+    //edition dui contenu de la balise contenu dans la avriable
+    baliseJsLoaded.innerHTML = "JS loaded";
+    baliseJsLoaded.className = "isLoaded";
+    console.log('Le js est chargé', baliseJsLoaded);
+}
+document.addEventListener('DOMContentLoaded',lorsDuChargementDeLaPage)
