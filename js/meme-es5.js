4282a85fcb218d83465f65f718a0b40c659a9515
champix56 <desorbaix@free.fr>
2022-03-30 11:12:20 +0100

    Instance + verou de forme

--- js/meme-es5.js
+++ js/meme-es5.js
@@ -2,29 +2,17 @@
  * constructeur d'un objet meme
  */
 function Meme(){
-    // champs public sur l'instance de this
-    this.name="Le nom";
-    this.texte="le text";
-    // fonction privé local a l'objet
-    function _unePrivateFn() {
-        console.log('hello');
-    }
-    // champs privé local(interne) à l'objet
-    var _unePrivateVar="private value"
-    //exposition public d'un fonction
-    this.maPublicFn=_unePrivateFn;
-    //fonction public
-    this.fn=function (uneValue) {
-        //acces au parametre d'entrée de la fonction fn
-        console.log(uneValue);
-        // acces au champs public de l'instance
-        console.log(this.name);
-        // acces a la fonction public de l'instance
-        this.maPublicFn();
-        // acces au champs privé de l'instance
-        console.log(_unePrivateVar);
-        // execution d'une fonction privé de l'instance
-        _unePrivateFn();
-    }
+    this.name='';
+    this.text='';
+    this.x=0;
+    this.y=0;
+    this.fontSize=12;
+    this.fontWeight='900';
+    this.underline=false;
+    this.italic=false;
+    this.color='#000000';
+    this.image='futurama-suicide.jpg';
+    console.log(this);
 }
-var unMemeGlobal=new Meme();
+// objet dont la forme ne PEUX PAS CHANGER
+var unMemeGlobal=Object.seal(new Meme());
